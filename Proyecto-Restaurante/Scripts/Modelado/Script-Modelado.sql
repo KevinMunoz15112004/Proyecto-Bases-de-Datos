0ec5116e05473114b79f1eb71817d892ae827d59
KevinMunoz15112004 <168014907+KevinMunoz15112004@users.noreply.github.com>
2025-02-02 20:43:20 -0500

    Add files via upload

---
+++ Proyecto-Restaurante/Scripts/Modelado/Script-Modelado.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE Restaurante;
+
+USE Restaurante;
+
+CREATE TABLE Cliente(
+	ClienteID INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(100) NOT NULL,
+    Apellido VARCHAR(100) NOT NULL,
+    Correo VARCHAR(100) UNIQUE NOT NULL,
+    Edad INT NOT NULL
+);
+
+CREATE TABLE Reserva(
+	ReservaID INT AUTO_INCREMENT PRIMARY KEY,
+    ClienteID INT NOT NULL,
+    FechaReserva TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    Estado ENUM("Confirmado", "Cancelado") NOT NULL,
+    CONSTRAINT fk_cliente_reserva FOREIGN KEY (ClienteID) REFERENCES Cliente(ClienteID) 
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE Pago(
+	PagoID INT AUTO_INCREMENT PRIMARY KEY,
+    ReservaID INT NOT NULL,
+    Cantidad DECIMAL(10, 2) NOT NULL,
+    MetodoPago ENUM("Tarjeta", "Efectivo") NOT NULL,
+    CONSTRAINT fk_pago_reserva FOREIGN KEY (ReservaID) REFERENCES Reserva(ReservaID)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE Mesa(
+	MesaID INT AUTO_INCREMENT PRIMARY KEY,
+    ReservaID INT NOT NULL,
+    Capacidad INT NOT NULL,
+    Estado ENUM("Libre", "Ocupado") NOT NULL,
+    CONSTRAINT fk_mesa_reserva FOREIGN KEY (ReservaID) REFERENCES Reserva(ReservaID)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
